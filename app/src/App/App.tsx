c7213160b93b74956edbde98ff6a2911543ab9a4
pmglg <paul.pmglg@gmail.com>
2025-12-16 15:52:45 +0100

    feat:ajout mock rest

--- app/src/App/App.tsx
+++ app/src/App/App.tsx
@@ -1,26 +1,36 @@
-import { emptyMeme, MemeSVGViewer } from "orsys-tjs-meme";
+import { emptyMeme, MemeSVGViewer, type ImageInterface } from "orsys-tjs-meme";
 import MemeForm from "../components/functionnal/MemeForm/MemeForm";
 import FlexH3Grow from "../components/layout/FlexH3Grow/FlexH3Grow";
 import FlexV1Grow from "../components/layout/FlexV1Grow/FlexV1Grow";
 import Footer from "../components/ui/Footer/Footer";
 import Header from "../components/ui/Header/Header";
 import Navbar from "../components/ui/Navbar/Navbar";
+import { useEffect, useState } from "react";
 
 const App: React.FC = () => {
+  const [images, setImages] = useState<Array<ImageInterface>>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5629/images")
+      .then((r) => r.json())
+      .then((imgs) => setImages(imgs));
+  }, []);
 
   return (
-    <div className="App">
-      <FlexH3Grow>
-        <Header />
-        <Navbar />
-        <FlexV1Grow>
-          <MemeSVGViewer meme={emptyMeme} image={undefined} basePath="" />
-          <MemeForm />
-        </FlexV1Grow>
-        <Footer></Footer>
-      </FlexH3Grow>
-
-    </div>
+    <>
+      <div>images:{JSON.stringify(images)}</div>
+      <div className="App">
+        <FlexH3Grow>
+          <Header />
+          <Navbar />
+          <FlexV1Grow>
+            <MemeSVGViewer meme={emptyMeme} image={undefined} basePath="" />
+            <MemeForm />
+          </FlexV1Grow>
+          <Footer></Footer>
+        </FlexH3Grow>
+      </div>
+    </>
   );
 };
 
